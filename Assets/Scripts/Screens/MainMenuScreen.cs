9e7a725cead0d46f197d810177013bdef9f3d612
Felipe Jun Fujioka Shida <felipe.fujioka@tapps.com.br>
2020-02-02 01:57:26 -0300

    home screen

---
+++ Assets/Scripts/Screens/MainMenuScreen.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Button = UnityEngine.UI.Button;
+
+public class MainMenuScreen : MonoBehaviour
+{
+    public void LoadJunScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
