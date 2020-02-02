78ed2e9884f6a1303d7fe63f712d11a1a8bf505f
Felipe Jun Fujioka Shida <felipe.fujioka@tapps.com.br>
2020-02-02 15:21:40 -0300

    fix sound and particles bugs

---
+++ Assets/HammerSFX.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HammerSFX : MonoBehaviour
+{
+    public void PlayHammerSound()
+    {
+        SoundManager.Instance.PlaySFX("sfx_tank_repair");   
+    }
+}
